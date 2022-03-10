1f50ab5564e23af1ec1acffc5645a7214810484e
Daniel <daniel.figueiredo1994@gmail.com>
2022-03-10 19:09:29 -0300

    Requisito 3 feito por Tuana e Daniel

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchBar extends Component {
   render() {
@@ -12,6 +13,13 @@ class SearchBar extends Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link
+          data-testid="shopping-cart-button"
+          to="/shoppingcart"
+        >
+          Carrinho de Compras
+        </Link>
+        <p>Você ainda não realizou nenhuma busca</p>
       </>
     );
   }
