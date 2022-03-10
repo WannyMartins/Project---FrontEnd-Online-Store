a3352d321d578f5598aef1a62f77ca63fa33f2c8
Daniel <daniel.figueiredo1994@gmail.com>
2022-03-10 16:30:20 -0300

    Requisito 1 fix2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import BrowserRouter from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
