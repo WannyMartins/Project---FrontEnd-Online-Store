77facaa0a1a3c54b49f2e7084d11a67cc838a600
erik-efl <erik.lima.f@outlook.com>
2022-03-11 14:44:46 -0300

    Adicionando requisito 4 ciração da category list :rocket:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import SearchBar from './Components/SearchBar';
+import Category from './Components/Category';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <BrowserRouter>
           <Route exact path="/" component={ SearchBar } />
+          <Category />
         </BrowserRouter>
       </div>
     );
